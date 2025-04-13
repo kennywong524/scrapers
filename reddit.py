6dee517fe5355ad218fa73ef0a5fa21e84be1e5b
Kenny Wongchamcharoen <pattaraphon.kenny@berkeley.edu>
2025-04-12 23:49:03 -0700

    fix API issue

--- reddit.py
+++ reddit.py
@@ -3,9 +3,9 @@ import csv
 
 # Initialize the Reddit instance
 reddit = praw.Reddit(
-    client_id='ypXSFJSJ_QpONh4yCaSzKw',
-    client_secret='80JuQPDLQPm8PVvWsU4zeMgfQ2XAJg',
-    user_agent='No_Bullfrog8687'
+    client_id='_',
+    client_secret='_',
+    user_agent='_'
 )
 
 def scrape_reddit_comments(url):
